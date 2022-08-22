f40216749e830dc76f9c0f22fa05431ea8f11dc4
Santiago Correa <santiagocorrez@gmail.com>
2022-08-22 18:51:14 -0500

    Subir Proyecto Spring

---
+++ src/main/java/com/elultimodeveloper/misiontic/MisionticApplication.java
@@ -0,0 +1,13 @@
+package com.elultimodeveloper.misiontic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisionticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisionticApplication.class, args);
+	}
+
+}
